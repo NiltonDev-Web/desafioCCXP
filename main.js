57f90f8d9557ea7951deb4db8b21ffc31d2489d4
NiltonDev-Web <jose.nilton2611@gmail.com>
2025-04-02 21:48:58 -0300

    Personalisar cartões

--- main.js
+++ main.js
@@ -1,12 +1,66 @@
 function addRedBorder(id)
 {
-    Element = document.querySelector("#" + id);
-    Element.style.border = "5px solid red";
+    element = document.querySelector("#" + id);
+    element.style.border = "5px solid red";
 }
 
 function highlightCard(selector) 
 {
     var element = document.querySelector(selector);
-    Element.classList.toggle("card-highlight");
+    element.classList.toggle("card-highlight");
     
 }
+function checkKeyboarCode()
+{
+    document.addEventListener("keydown", (event)=> {
+        var name = event.key;
+        var code = event.code;
+        //Alert the Key name and code on Keydown
+        alert('Tecla Pressionada ${name} \r\n Key code: ${code}');
+    },false);
+}
+
+function addKeyboardEventListeners()
+{
+    document.addEventListener("keydown", (event)=> {
+        var ingresso1 = document.getElementById("quinta");  
+        var ingresso2 = document.getElementById("sexta");
+        var ingresso3 = document.getElementById("sabado");     
+        var ingresso4 = document.getElementById("domingo");
+
+
+       
+        var code = event.code;
+        if(code =='Digit1') {
+            ingresso1.classList.toggle("card-highlight");
+            ingresso2.classList.remove("card-highlight");
+            ingresso3.classList.remove("card-highlight");
+            ingresso4.classList.remove("card-highlight");
+        }
+
+        if(code =='Digit2') {
+            ingresso1.classList.remove("card-highlight");
+            ingresso2.classList.toggle("card-highlight");
+            ingresso3.classList.remove("card-highlight");
+            ingresso4.classList.remove("card-highlight");
+        }
+
+        if(code =='Digit3') {
+            ingresso1.classList.remove("card-highlight");
+            ingresso2.classList.remove("card-highlight");
+            ingresso3.classList.toggle("card-highlight");
+            ingresso4.classList.remove("card-highlight");
+        }
+
+
+        if(code =='Digit4') {
+            ingresso1.classList.remove("card-highlight");
+            ingresso2.classList.remove("card-highlight");
+            ingresso3.classList.remove("card-highlight");
+            ingresso4.classList.toggle("card-highlight");
+        }
+
+
+}, false);
+}
+addKeyboardEventListeners();
